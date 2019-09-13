ae74a6977f5c9ebbd2b0809b0ba6089c6f621898
SJ Emmons <semmons@mac.com>
2019-09-13 19:46:06 +0100

    Added jQuery events Added jQuery event to h1

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,3 +1,9 @@
+// for (let i = 0; i < 5; i++) {
+//   document.querySelectorAll("button")[i].addEventListener("click", function() {
+//     document.querySelector("h1").style.color = "purple";
+//   });
+// }
 
-
-$("a").attr("href", "https://www.yahoo.com");
+$("h1").on("click", function() {
+  $("h1").css("color", "purple");
+});
