00d6b05a7a54602ad929bf37450b7a7bdbf9149c
SJ Emmons <semmons@mac.com>
2019-09-21 21:55:57 +0100

    Add game start Add game start functionality

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,14 +1,56 @@
 
 const buttonColors = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
+let userClickedPattern = [];
+let gameStarted = false;
+let level = 0;
+let levelTitle = $("#level-title");
 
+function playSound(color) {
+  if (color > "") {
+    var audio = new Audio("sounds/" + color + ".mp3");
+    audio.play();
+  }
+}
 function nextSequence() {
   let randomNumber;
 
   randomNumber = Math.floor(Math.random() * 4);
-  return randomNumber;
+  let randomChosenColour = buttonColors[randomNumber];
+
+  level++;
+  levelTitle.text("Level " + level);
+
+  gamePattern.push(randomChosenColour);
+
+  let button = $("#" + randomChosenColour);
+
+  // flash the button
+  button.fadeOut(100).fadeIn(100);
+
+  playSound(randomChosenColour);
 }
 
-let randomChosenColour = buttonColors[nextSequence()];
+$(".btn").on("click", function() {
+  var userChosenColor = this.id;
+  userClickedPattern.push(userChosenColor);
+  playSound(userChosenColor);
+  animatePress(userChosenColor);
+});
 
-gamePattern.push(randomChosenColour);
+// check key pressed then call nextSequence
+// need a way of tracking if game started, only call nextSequence on first keypress
+$(document).keypress(event => {
+  if (!gameStarted) {
+    gameStarted = true;
+    levelTitle.text("Level " + level);
+    nextSequence();
+  }
+});
+
+function animatePress(color) {
+  $("#" + color).addClass("pressed");
+  setTimeout(() => {
+    $("#" + color).removeClass("pressed");
+  }, 100);
+}
