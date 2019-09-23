f3932ee3bf0bddd01388488db2a28d72c7ca9696
SJ Emmons <semmons@mac.com>
2019-09-23 19:25:04 +0100

    Finish game development

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -21,14 +21,16 @@ function nextSequence() {
   level++;
   levelTitle.text("Level " + level);
 
+  userClickedPattern = [];
+
   gamePattern.push(randomChosenColour);
 
   let button = $("#" + randomChosenColour);
 
   // flash the button
   button.fadeOut(100).fadeIn(100);
-
   playSound(randomChosenColour);
+
 }
 
 $(".btn").on("click", function() {
@@ -36,14 +38,24 @@ $(".btn").on("click", function() {
   userClickedPattern.push(userChosenColor);
   playSound(userChosenColor);
   animatePress(userChosenColor);
+
+  // check answer against game pattern
+  if (gameStarted) {
+    checkAnswer(userClickedPattern.length - 1);
+  }
 });
 
 // check key pressed then call nextSequence
 // need a way of tracking if game started, only call nextSequence on first keypress
 $(document).keypress(event => {
   if (!gameStarted) {
+    // initialise game
     gameStarted = true;
+    level = 0;
+    gamePattern = [];
     levelTitle.text("Level " + level);
+
+    // start game
     nextSequence();
   }
 });
@@ -53,4 +65,36 @@ function animatePress(color) {
   setTimeout(() => {
     $("#" + color).removeClass("pressed");
   }, 100);
+}
+
+function checkAnswer(currentLevel) {
+  // check user pattern equals game pattern
+  // if true, go to next sequence
+  // if false, the game ends
+
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log("gamePattern === userClickedPattern");
+
+    if (currentLevel + 1 == level) {
+      setTimeout(() => {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    playSound("wrong");
+
+    $("body").addClass("game-over");
+    setTimeout(() => {
+      $("body").removeClass("game-over");
+    }, 200);
+
+    levelTitle.text("Game Over, Press Any Key to Restart")
+    startOver();
+  }
+}
+
+function startOver() {
+  gameStarted = false;
+  level = 0;
+  gamePattern = [];
 }
